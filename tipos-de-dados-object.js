4f7b782029309d68693739b8477edeb54b8fa487
senacmatheus2026-cell <271594164+Aguiar-Gabys@users.noreply.github.com>
2026-09-08 17:17:33 -0300

    add tipos de dados object

---
+++ tipos-de-dados-object.js
@@ -0,0 +1,11 @@
+function exemploObject () {
+    let pessoas = {
+        nome: "Davi Carlos",
+        idade: 15,
+        casado: false,
+    };
+    // acessando propriedades do objeto
+    console.log(pessoas.nome); //"Davi Carlos"
+    console.log(pessoa.idade); // 15
+    
+}
