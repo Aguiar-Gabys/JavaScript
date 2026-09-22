8bba48612a5fcbf726e0440ad4327350843a4bf9
senacmatheus2026-cell <puguerorobson@gmail.com>
2026-09-22 15:08:45 -0300

    add if

--- if.js
+++ if.js
@@ -4,4 +4,4 @@ if (hora < 12) {
     console.log("Bom dia!");
 }
 // O restante do código continua independentemente se a condição for verdadeira ou não.
-console.log("Dinheiro não traz felicidade. Me dê o seu e seja feliz!")
+console.log("Dinheiro não traz felicidade. Me dê o seu e seja feliz!");
